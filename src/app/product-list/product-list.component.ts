2a4c953ad1abdb1957ccbd7acea8f65612d71a4f
programCodesudo <brandonjose2001@hotmail.com>
2024-01-28 19:07:49 -0600

    stacblitz project

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+
+export class ProductListComponent {
+  products = [...products];
+
+  share() {
+    window.alert('The product has been shared!');
+  }
+
+  onNotify(){
+    window.alert('The product is big cost 700 >>')
+  }
+}
